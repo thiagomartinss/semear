02be66ebf9b1a70dcf79f65076d76a94cdd57ed7
Thiago Martins <thiagomartinsdsantos@gmail.com>
2025-10-13 17:54:57 -0300

    inclusion of the trademark registration

--- controllers/marcaController.js
+++ controllers/marcaController.js
@@ -7,5 +7,32 @@ class MarcaController{
         let lista = await marca.listarMarcas();
         res.render('marca/listarMarca', {lista: lista});
     }
+
+    async cadastrar(req, resp){
+        let msg = "";
+        let cor = "";
+
+        if(req.body.descricao != ""){
+            let marca = new MarcaModel(0, req.body.descricao);
+            let result = await marca.cadastrarMarcas();
+
+            if(result){
+                resp.send({
+                    ok: true,
+                    msg: "Marca cadastrada com sucesso!"
+                });
+            }else{
+                resp.send({
+                    ok: false,
+                    msg: "Erro ao cadastrar a marca!"
+                });
+            }
+        }else{
+            resp.send({
+                ok: false,
+                msg: "Preencha o campo!"
+            });
+        }
+    }
 }
 module.exports = MarcaController;
